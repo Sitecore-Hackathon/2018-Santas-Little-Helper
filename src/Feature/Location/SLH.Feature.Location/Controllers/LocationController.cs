fbb946d76578697de7b90ce4b604554da20be195
HQ\Santiago.Morla <santiago.morla@verndale.com>
2018-03-03 12:32:20 -0500

    Refactor code to use constants

--- src/Feature/Location/SLH.Feature.Location/Controllers/LocationController.cs
+++ src/Feature/Location/SLH.Feature.Location/Controllers/LocationController.cs
@@ -1,14 +1,66 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Sitecore.Data.Fields;
+using Sitecore.Mvc.Controllers;
 using Sitecore.Mvc.Presentation;
+using Sitecore.Resources.Media;
+using SLH.Feature.Location.Models;
 
 namespace SLH.Feature.Location.Controllers
 {
-    public class LocationController : Controller
+    public class LocationController : SitecoreController
     {
         public ActionResult LocationMap()
         {
             var model = RenderingContext.Current.Rendering.Item;
-            return View(model);
+
+            if (model == null)
+            {
+                return new EmptyResult();
+            }
+
+            var locationMapModel = new LocationMapViewModel();
+
+            ImageField image = model.Fields[Constants.FieldId.LocationMapImage];
+            if (image?.MediaItem != null)
+            {
+                locationMapModel.ImageUrl = MediaManager.GetMediaUrl(image.MediaItem);
+                locationMapModel.ImageHeight = Convert.ToInt32(image.Height);
+                locationMapModel.ImageWidth = Convert.ToInt32(image.Width);
+            }
+
+            var poiItemsList = new List<PoiItem>();
+            MultilistField poiItems = model.Fields[Constants.FieldId.LocationMapPois];
+            if (poiItems?.TargetIDs != null)
+            {
+                foreach (var poi in poiItems.GetItems())
+                {
+                    var title = poi.Fields[Constants.FieldId.PoiTitle].Value;
+
+                    var poiItem = new PoiItem
+                    {
+                        Title = title,
+                        Top = Convert.ToDecimal(poi.Fields[Constants.FieldId.PoiTop].Value),
+                        Left = Convert.ToDecimal(poi.Fields[Constants.FieldId.PoiLeft].Value),
+                        Id = poi.ID.Guid
+                    };
+
+                    ReferenceField iconField = poi.Fields[Constants.FieldId.PoiType];
+                    var icon = Constants.FieldValue.PoiTypeIconClass;
+                    if (iconField?.TargetItem != null)
+                    {
+                        icon = iconField.TargetItem.Fields[Constants.FieldId.PoiTypeIconClass].Value;
+                    }
+                    poiItem.IconClass = icon;
+
+                    poiItemsList.Add(poiItem);
+                }
+            }
+
+            locationMapModel.PoiItems = poiItemsList;
+
+            return View(locationMapModel);
         }
     }
 }
---
+++ src/Feature/Location/SLH.Feature.Location/Models/PoiItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SLH.Feature.Location.Models
+{
+    public class PoiItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string IconClass { get; set; }
+        public decimal Top { get; set; }
+        public decimal Left { get; set; }
+    }
+}
